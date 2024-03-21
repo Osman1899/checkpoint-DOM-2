194801f17b818b6526d1e01ebf541b205cd05e78
Ahmad Khaled Osman <158057636+Osman1899@users.noreply.github.com>
2024-03-21 09:38:07 +0000

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionnez les éléments color-box et change-color-btn
     var colorBox = document.getElementById('color-box');
     var changeColorBtn = document.getElementById('change-color-btn');
-    
-    // Implémentez une fonction pour générer une couleur aléatoire
     function getRandomColor() {
       var letters = '0123456789ABCDEF';
       var color = '#';
@@ -13,9 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
       return color;
     }
     
-    // Ajoutez un écouteur d'événement au bouton change-color-btn
     changeColorBtn.addEventListener('click', function() {
-      // Changez la couleur de fond de la boîte de couleur en une couleur aléatoire
       colorBox.style.backgroundColor = getRandomColor();
     });
   });
